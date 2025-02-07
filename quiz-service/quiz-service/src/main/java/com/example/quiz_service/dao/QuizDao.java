3f5ebda7433ed11e3bfc6ced6b1353f7b480b15d
Prashant Kumar <164150104+prashant0240@users.noreply.github.com>
2025-02-08 02:19:30 +0530

    Delete quiz-service/quiz-service directory

--- quiz-service/quiz-service/src/main/java/com/example/quiz_service/dao/QuizDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.quiz_service.dao;
-
-
-import com.example.quiz_service.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizDao extends JpaRepository<Quiz, Integer> {
-}
